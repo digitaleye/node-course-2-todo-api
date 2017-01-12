ed7ce7aace7eba105c812fec0f9f041523f7df61
ctsuser1 <jatoi.fahar.kp.org>
2017-01-12 10:41:31 -0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,9 +17,11 @@ app.post('/todos', (req,res)=>{
     todo.save().then((doc)=>{
         res.send(doc);
     }, (e)=>{
-        res.send(e)
+        res.status(400).send(e)
     })
 })
+
+module.exports = {app};
 
 
 app.listen(3000, () =>{
