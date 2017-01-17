c3e26954b0809412b5303bf0b99cdf07c92b5da0
ctsuser1 <jatoi.fahar.kp.org>
2017-01-17 00:02:46 -0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-    id: 10
-}
+var password = '123abc!'
 
-var token = jwt.sign(data, '123abc')
-console.log(token);
+bcrypt.genSalt(10,(err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash)
+    });
+});
 
-var decoded = jwt.verify(token, '123abc')
-console.log('decoded', decoded)
+var hashedPassword = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODdkYjVhN2RkZWQ5MTllMTFiYjY0MjgiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNDg0NjMzNTExfQ.RbJLxlP_nzyOYPt6xvTEZA-SzE5JopV36DmlONM6fCg'
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+    console.log(res)
+})
+
+// var data = {
+//     id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc')
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc')
+// console.log('decoded', decoded)
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
